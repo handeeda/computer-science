34161e2e8c8d3e5345ad142b3efc4589d9244615
handeeda <144910880+handeeda@users.noreply.github.com>
2023-10-06 12:28:37 +0100

    Add files via upload

---
+++ if else, for loops.py
@@ -0,0 +1,79 @@
+"""
+ name=input("enter youre name:")
+number= int(input("enter a number:"))
+for a in range(number):
+ for b in (name):
+  print (b)
+
+number=int(input("enter a number:"))
+if number<12 and number>1:
+    for a in range (number):
+     for b in range(number):
+         print(number)
+elif number>12:
+    print("too high")
+else:
+    print ("too low")
+    "
+number=int(input("enter a number below 50:"))
+if number>50:
+    for a in range(number,49,-1):
+        print(a)
+else:
+    print("wrong answer")
+
+name= input("enter youre name:")
+number=int(input("enter a number:"))
+if number<10:
+    for a in range(number):
+            print(name)
+else:
+    for c in range(3):
+     print("too high")
+
+drn=input("up or down")
+a="up"
+b="down"
+if drn==a:
+    top=int(input("enter the top number:"))
+    for m in range (1,top):
+        print(m)
+elif drn==b:
+    number=int(input("enter a number below 20:"))
+    for d in range(number,20,-1):
+        print(d)
+else:
+    print("i dont understand")
+    
+number= int(input("how many people do you wanna invite"))
+if number<10:
+    
+    for a in range(number):
+     name=input("enter the names")
+     print(name, "has invited")
+else:
+    print("too many people")
+    """
+    
+    
+    
+
+
+        
+        
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
